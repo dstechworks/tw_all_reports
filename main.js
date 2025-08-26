6ce4b718ebf3fd7170999d084cc22c8efe06d325
hiteshtechworks <144338235+hiteshtechworks@users.noreply.github.com>
2025-08-26 16:21:31 +0530

    code added !!

---
+++ main.js
@@ -0,0 +1,47 @@
+
+const { exec } = require('child_process');
+const moment = require('moment-timezone');
+const cron = require('node-cron');
+
+const TIMEZONE = 'Asia/Kolkata';
+
+// Print message with current time
+function logWithTime(message) {
+    const time = moment().tz(TIMEZONE).format('YYYY-MM-DD HH:mm:ss');
+    console.log(`[${time}] ${message}`);
+}
+
+// Run a script using node
+function runScript(name, path) {
+    logWithTime(`Starting ${name}...`);
+    exec(`node ${path}`, (error, stdout, stderr) => {
+        if (error) {
+            logWithTime(`${name} ERROR: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            logWithTime(`${name} STDERR: ${stderr}`);
+        }
+        logWithTime(`${name} OUTPUT: ${stdout.trim()}`);
+    });
+}
+
+// Every Monday @ 11:58 AM: Run MPDU Complaints
+cron.schedule('58 11 * * 1', () => {
+    runScript('MPDU Complaints', 'src/mpdu/mpdu_43vertical_complaint_report/mpduComplaints.js'); // Monday 11:58 AM
+}, { timezone: TIMEZONE });
+
+// 15th day of month and last day of month @ 12:00 PM: Run Tab Complaints
+cron.schedule('0 12 15 * *', () => {
+    runScript('Tab Complaints', 'src/tabs/tabComplaints.js'); // 15th day of month at 12 PM
+}, { timezone: TIMEZONE });
+
+cron.schedule('0 12 L * *', () => {
+    runScript('Tab Complaints', 'src/tabs/tabComplaints.js'); // Last day of month at 12 PM
+}, { timezone: TIMEZONE });
+
+// Run a script manually if i want to runscript manually without using cron
+// runScript('MPDU Complaints', 'src/mpdu/mpdu_43vertical_complaint_report/mpduComplaints.js');
+// runScript('Tab Complaints', 'src/tabs/tabComplaints.js');
+// runScript('MPDU 43 Vertical', 'src/mpdu/mpdu_43vertical_month_end_report/mpdu.js');
+// runScript('MPDU 43 Vertical', 'src/mpdu/mpdu_43vertical_month_end_report/43vertical.js');
