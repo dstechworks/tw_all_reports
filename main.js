be32714fb44a520d038ce3c0300efe4ab1285d4d
hiteshtechworks <144338235+hiteshtechworks@users.noreply.github.com>
2025-08-26 17:56:11 +0530

    live testing !!

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ cron.schedule('0 12 L * *', () => {
 }, { timezone: TIMEZONE });
 
 // Run a script manually if i want to runscript manually without using cron
-// runScript('MPDU Complaints', 'src/mpdu/mpdu_43vertical_complaint_report/mpduComplaints.js');
+runScript('MPDU Complaints', 'src/mpdu/mpdu_43vertical_complaint_report/mpduComplaints.js');
 // runScript('Tab Complaints', 'src/tabs/tabComplaints.js');
 // runScript('MPDU 43 Vertical', 'src/mpdu/mpdu_43vertical_month_end_report/mpdu.js');
 // runScript('MPDU 43 Vertical', 'src/mpdu/mpdu_43vertical_month_end_report/43vertical.js');
