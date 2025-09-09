29c23560caeb8937013ec47cd6132b6e6457beb0
hiteshtechworks <144338235+hiteshtechworks@users.noreply.github.com>
2025-09-09 15:37:19 +0530

    email signature updated !!

--- src/mpdu/mpdu_43vertical_month_end_report/43vertical.js
+++ src/mpdu/mpdu_43vertical_month_end_report/43vertical.js
@@ -918,98 +918,92 @@ async function sendReportEmail(params) {
             subject: "MPDU REPORT TILL" + current_date, // Subject line
             html: `<h6>Please find the attachment.</h6>
             <p>&nbsp;</p>
-            <table style="width: 420px; font-size: 10pt; font-family: Verdana, sans-serif; background: transparent !important;"
-                    border="0" cellspacing="0" cellpadding="0">
-                    <tbody>
-                            <tr>
-                                    <td style="width: 160px; font-size: 10pt; font-family: Verdana, sans-serif; vertical-align: top;"
-                                            valign="top">
-                                            <p style="margin-bottom: 18px; padding: 0px;"><span
-                                                            style="font-size: 12pt; font-family: Verdana, sans-serif; color: #183884; font-weight: bold;"><a
-                                                                    href="http://contact.techworksworld.com/" target="_"><img
-                                                                            style="width: 120px; height: auto; border: 0;"
-                                                                            src="https://i.imgur.com/Eie8F53.png" width="120"
-                                                                            border="0" /></a></p>
-                                            <p
-                                                    style="margin-bottom: 0px; padding: 0px; font-family: Verdana, sans-serif; font-size: 9pt; line-height: 12pt;">
-                                                    <a style="color: #e25422; text-decoration: none; font-weight: bold;"
-                                                            href="http://www.techworksworld.com" target="_"><span
-                                                                    style="text-decoration: none; font-size: 9pt; line-height: 12pt; color: #e25422; font-family: Verdana, sans-serif; font-weight: bold;">www.techworksworld.com</span></a>
-                                            </p>
-                                    </td>
-                                    <td style="width: 30px; min-width: 30px; border-right: 1px solid #e25422;">&nbsp;</td>
-                                    <td style="width: 30px; min-width: 30px;">&nbsp;</td>
-                                    <td style="width: 200px; font-size: 10pt; color: #444444; font-family: Verdana, sans-serif; vertical-align: top;"
-                                            valign="top">
-                                            <p
-                                                    style="font-family: Verdana, sans-serif; padding: 0px; font-size: 9pt; line-height: 14pt; margin-bottom: 14px;">
-                                                    <span
-                                                            style="font-family: Verdana, sans-serif; font-size: 9pt; line-height: 14pt;"><span
-                                                                    style="font-size: 9pt; line-height: 13pt; color: #262626;"><strong>E:
-                                                                    </strong></span><a
-                                                                    style="font-size: 9pt; color: #262626; text-decoration: none;"
-                                                                    href="mailto:support@techworks.co.in"><span
-                                                                            style="text-decoration: none; font-size: 9pt; line-height: 14pt; color: #262626; font-family: Verdana, sans-serif;">support@techworks.co.in</span></a><span><br /></span></span><span><span
-                                                                    style="font-size: 9pt; color: #262626;"><strong>T:</strong></span><span
-                                                                    style="font-size: 9pt; color: #262626;"> (+91) 11 35007205</span><span><br /></span></span><span><span
-                                                                    style="font-size: 9pt; color: #262626;"><strong>A:</strong></span><span
-                                                                    style="font-size: 9pt; color: #262626;"> O-7,Second Floor Lajpat
-                                                                    Nagar-II,<span>,</span></span><span style="color: #262626;">New
-                                                                    Delhi-110024,India</span></span></p>
-                                            <p style="margin-bottom: 0px; padding: 0px;"><span><a
-                                                                    href="https://www.facebook.com/TechworksSolutionsPvtLtd/"
-                                                                    rel="noopener"><img
-                                                                            style="border: 0; height: 22px; width: 22px;"
-                                                                            src="https://www.mail-signatures.com/signature-generator/img/templates/inclusive/fb.png"
-                                                                            width="22" border="0" /></a>&nbsp;</span><span><a
-                                                                    href="https://www.linkedin.com/company/ds-techworks-solutions-pvt-ltd/"
-                                                                    rel="noopener"><img
-                                                                            style="border: 0; height: 22px; width: 22px;"
-                                                                            src="https://www.mail-signatures.com/signature-generator/img/templates/inclusive/ln.png"
-                                                                            width="22" border="0" /></a>&nbsp;</span><span><a
-                                                                    href="https://twitter.com/techworks14" rel="noopener"><img
-                                                                            style="border: 0; height: 22px; width: 22px;"
-                                                                            src="https://www.mail-signatures.com/signature-generator/img/templates/inclusive/tt.png"
-                                                                            width="22" border="0" /></a>&nbsp;</span><span><a
-                                                                    href="https://www.youtube.com/@TechworksDigitalSolutions"
-                                                                    rel="noopener"><img
-                                                                            style="border: 0; height: 22px; width: 22px;"
-                                                                            src="https://www.mail-signatures.com/signature-generator/img/templates/inclusive/yt.png"
-                                                                            width="22" border="0" /></a>&nbsp;</span><span><a
-                                                                    href="https://www.instagram.com/techworks140/"
-                                                                    rel="noopener"><img
-                                                                            style="border: 0; height: 22px; width: 22px;"
-                                                                            src="https://www.mail-signatures.com/signature-generator/img/templates/inclusive/it.png"
-                                                                            width="22" border="0" /></a></span></p>
-                                    </td>
-                            </tr>
-                            <tr style="width: 420px;">
-                                    <td style="padding-top: 14px;" colspan="4"><a href="https://techworksworld.com/" target="_"><img
-                                                            style="width: 420px; height: auto; border: 0;"
-                                                            src="https://i.imgur.com/QoPxSPy.png" width="420" border="0" /></a></td>
-                            </tr>
-                            <tr>
-                                    <td style="padding-top: 14px; text-align: justify;" colspan="4">
-                                            <table style="width: 420px; font-size: 10pt; font-family: Verdana, sans-serif; background: transparent !important;"
-                                                    border="0" cellspacing="0" cellpadding="0">
-                                                    <tbody>
-                                                            <tr>
-                                                                    <td
-                                                                            style="font-size: 8pt; color: #b2b2b2; line-height: 9pt; text-align: justify;">
-                                                                            The content of this email is confidential and intended
-                                                                            for the recipient specified in message only. It is
-                                                                            strictly forbidden to share any part of this message
-                                                                            with any third party,without a written consent of the
-                                                                            sender. If you received this message by mistake,please
-                                                                            reply to this message and follow with its deletion,so
-                                                                            that we can ensure such a mistake does not occur in the
-                                                                            future.</td>
-                                                            </tr>
-                                                    </tbody>
-                                            </table>
-                                    </td>
-                            </tr>
-                    </tbody>
+            <table style="width:450px; font-size: 10pt; font-family: Verdana, sans-serif; background: transparent !important;"
+                border="0" cellspacing="0" cellpadding="0">
+                <tbody>
+                    <tr>
+                        <td style="width: 200px; font-size: 10pt; font-family: Verdana, sans-serif; vertical-align: top;"
+                            valign="top">
+                            <p style="margin-bottom: 18px; padding: 0px;"><span
+                                    style="font-size: 12pt; font-family: Verdana, sans-serif; color: #183884; font-weight: bold;">Techworks
+                                    Reports<br /></span><span
+                                    style="font-family: Verdana, sans-serif; font-size: 9pt; color: #183884;">DS Techworks
+                                    Solutions</span></p>
+                            <p style="margin-top: 0px; margin-bottom: 18px; padding: 0px;"><a
+                                    href="http://www.vcard.techworksworld.com/techworks_reports/" target="_blank"><img
+                                        style="width: 120px; height: auto; border: 0;"
+                                        src="https://raw.githubusercontent.com/tw-designer/tw-emp-qr-links/main/qr_techworks_reports.png"
+                                        width="120" border="0" /></a></p>
+                            <p
+                                style="margin-bottom: 0px; padding: 0px; font-family: Verdana, sans-serif; font-size: 9pt; line-height: 12pt;">
+                                <a style="color: #e25422; text-decoration: none; font-weight: bold;"
+                                    href="http://www.techworksworld.com" rel="noopener"><span
+                                        style="text-decoration: none; font-size: 9pt; line-height: 12pt; color: #e25422; font-family: Verdana, sans-serif; font-weight: bold;">www.techworksworld.com</span></a>
+                            </p>
+                        </td>
+                        <td style="width: 10px; min-width: 10px; border-right: 1px solid #e25422;">&nbsp;</td>
+                        <td style="width: 10px; min-width: 10px;">&nbsp;</td>
+                        <td style="width: 250px; font-size: 10pt; color: #444444; font-family: Verdana, sans-serif; vertical-align: top;"
+                            valign="top">
+                            <p
+                                style="font-family: Verdana, sans-serif; padding: 0px; font-size: 9pt; line-height: 14pt; margin-bottom: 14px;">
+                                <span style="font-family: Verdana, sans-serif; font-size: 9pt; line-height: 14pt;"><span
+                                        style="font-size: 9pt; line-height: 13pt; color: #262626;"><strong>E: </strong></span><a
+                                        style="font-size: 9pt; color: #262626; text-decoration: none;"
+                                        href="mailto:reports@techworks.co.in"><span
+                                            style="text-decoration: none; font-size: 9pt; line-height: 14pt; color: #262626; font-family: Verdana, sans-serif;">reports@techworks.co.in</span></a><span><br /></span></span><span><span
+                                        style="font-size: 9pt; color: #262626;"><strong>T:</strong></span><span
+                                        style="font-size: 9pt; color: #262626;">(+91)
+                                        8920131195</span><span><br /></span></span><span><span
+                                        style="font-size: 9pt; color: #262626;"><strong>A:</strong></span><span
+                                        style="font-size: 9pt; color: #262626;"> O-7, 2nd Floor Lajpat Nagar-II, </span><span
+                                        style="color: #262626;">New Delhi-110024, India</span></span></p>
+                            <p style="margin-bottom: 0px; padding: 0px;"><span><a
+                                        href="https://www.facebook.com/TechworksSolutionsPvtLtd/" rel="noopener"><img
+                                            style="border: 0; height: 22px; width: 22px;"
+                                            src="https://www.mail-signatures.com/signature-generator/img/templates/inclusive/fb.png"
+                                            width="22" border="0" /></a>&nbsp;</span><span><a
+                                        href="https://www.linkedin.com/company/ds-techworks-solutions-pvt-ltd/" rel="noopener"><img
+                                            style="border: 0; height: 22px; width: 22px;"
+                                            src="https://www.mail-signatures.com/signature-generator/img/templates/inclusive/ln.png"
+                                            width="22" border="0" /></a>&nbsp;</span><span><a href="https://twitter.com/techworks14"
+                                        rel="noopener"><img style="border: 0; height: 22px; width: 22px;"
+                                            src="https://www.mail-signatures.com/signature-generator/img/templates/inclusive/tt.png"
+                                            width="22" border="0" /></a>&nbsp;</span><span><a
+                                        href="https://www.youtube.com/@TechworksDigitalSolutions" rel="noopener"><img
+                                            style="border: 0; height: 22px; width: 22px;"
+                                            src="https://www.mail-signatures.com/signature-generator/img/templates/inclusive/yt.png"
+                                            width="22" border="0" /></a>&nbsp;</span><span><a
+                                        href="https://www.instagram.com/techworks140/" rel="noopener"><img
+                                            style="border: 0; height: 22px; width: 22px;"
+                                            src="https://www.mail-signatures.com/signature-generator/img/templates/inclusive/it.png"
+                                            width="22" border="0" /></a></span></p>
+                        </td>
+                    </tr>
+                    <tr style="width: 420px;">
+                        <td style="padding-top: 14px;" colspan="4"><a href="https://techworksworld.com/" rel="noopener"><img
+                                    style="width: 420px; height: auto; border: 0;" src="https://i.imgur.com/QoPxSPy.png" width="420"
+                                    border="0" /></a></td>
+                    </tr>
+                    <tr>
+                        <td style="padding-top: 14px; text-align: justify;" colspan="4">
+                            <table
+                                style="width: 420px; font-size: 10pt; font-family: Verdana, sans-serif; background: transparent !important;"
+                                border="0" cellspacing="0" cellpadding="0">
+                                <tbody>
+                                    <tr>
+                                        <td style="font-size: 8pt; color: #b2b2b2; line-height: 9pt; text-align: justify;">The
+                                            content of this email is confidential and intended for the recipient specified in
+                                            message only. It is strictly forbidden to share any part of this message with any third
+                                            party,without a written consent of the sender. If you received this message by
+                                            mistake,please reply to this message and follow with its deletion,so that we can ensure
+                                            such a mistake does not occur in the future.</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                </tbody>
             </table>`,
             attachments: [
                 {   // file on disk as an attachment
@@ -1235,7 +1229,7 @@ Promise.all([querydb()])
                     if (!fs.existsSync(reportsFolderPath)) {
                         fs.mkdirSync(reportsFolderPath);
                     }
-                    
+
                     const buffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'buffer' });
                     const fileName = '43 Vertical REPORT TILL' + current_date + '.xlsx'
                     XLSX.writeFile(workbook, path.join(reportsFolderPath, fileName));
